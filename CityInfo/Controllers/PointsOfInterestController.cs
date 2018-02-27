c6388858281e3cfb76df8a9d31a90975c200d521
kbussert <32082173+kbussert@users.noreply.github.com>
2018-02-27 16:33:42 -0500

    Added logging functionality.  Still needs work.

--- CityInfo/Controllers/PointsOfInterestController.cs
+++ CityInfo/Controllers/PointsOfInterestController.cs
@@ -5,23 +5,43 @@ using System.Linq;
 using System.Threading.Tasks;
 using CityInfo.Models;
 using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.Extensions.Logging;
 
 namespace CityInfo.Controllers
 {
     [Route("api/cities")]
     public class PointsOfInterestController : Controller
     {
+        private ILogger<PointsOfInterestController> _logger;
+
+        public PointsOfInterestController(ILogger<PointsOfInterestController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet("{cityId}/pointsofinterest")]
         public IActionResult GetPointsOfInterest(int cityId)
         {
-            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
-            
-            if (city == null)
+            try
             {
-                return NotFound();
+                throw new Exception("Sample Exception");
+
+                var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+                if (city == null)
+                {
+                    _logger.LogInformation($"City with id {cityId} wasn't found when accessing points of interest.");
+                    return NotFound();
+                }
+
+                return Ok(city.PointsOfInterest);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical($"Exception while getting points of interest for city with id {cityId}.", ex);
+                return StatusCode(500, "A problem happened while handling your request.");
             }
 
-            return Ok(city.PointsOfInterest);
         }
 
         [HttpGet("{cityId}/pointsofinterest/{id}", Name ="GetPointOfInterest")]
