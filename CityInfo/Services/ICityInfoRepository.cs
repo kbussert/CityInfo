3b2663664183c8327994254a66748aea2b253861
Kyle Bussert <Kbuzz24@hotmail.com>
2018-03-16 12:19:24 -0400

    Started implementation of repository pattern for using EF and SQLServer rather than in memory data store.

---
+++ CityInfo/Services/ICityInfoRepository.cs
@@ -0,0 +1,16 @@
+﻿using CityInfo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.Services
+{
+    public interface ICityInfoRepository
+    {
+        IEnumerable<City> GetCities();
+        City GetCity(int cityId, bool includePointsOfInterest);
+        IEnumerable<PointOfInterest> GetPointOfInterestForCity(int cityId);
+        PointOfInterest GetPointOfInterest(int cityId, int pointOfInterestId);
+    }
+}
